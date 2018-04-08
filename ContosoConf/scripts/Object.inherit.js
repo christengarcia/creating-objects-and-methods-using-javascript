6209531cf5488c909118e8a9d422a0c538449a11
Christen Garcia <christen.garcia@gmail.com>
2018-04-09 04:50:51 +1000

    Add Object.inherit utility function

--- ContosoConf/scripts/Object.inherit.js
+++ ContosoConf/scripts/Object.inherit.js
@@ -1,7 +1,7 @@
 ﻿// TODO: Wrap this file's code in an immediately invoked function expression
-
+(function () {
 // TODO: Strict mode
-
+    "use strict";
 var copyOwnProperties = function (from, to) {
     for (var propertyName in from) {
         if (from.hasOwnProperty(propertyName)) {
@@ -20,24 +20,31 @@ var inherit = function (additionalProperties) {
     // called to initialize created instances.
     
     // TODO: Create a variable named `factory`, assign it a new object who's prototype is `this`.
+    var factory = Object.create(this);
     
     // TODO: Add a method called `create` to `factory`, that does the following
-    
+    factory.create = function() {
         // TODO: Define a variable named `instance` 
         //       and assign it a new object that has `factory` as its prototype.
-    
+        var instance = Object.create(factory);
+
         // TODO: If `instance` has a function named "initialize",
         //       then call `initialize`, passing any arguments passed to `create`.
-    
+        if (typeof instance.initialize === "function") {
+            instance.initialize.apply(instance, arguments);
+        }
         // TODO: return `instance`.
-    
+        return instance;
 
     // TODO: Copy properties of `additionalProperties` onto `factory' (using copyOwnProperties).
     
-    // TODO: Return the `factory` object.
+        // TODO: Return the `factory` object.
+        return factory;
 };
 
-// TODO: Add the inherit function to the built-in `Object` object.
+    // TODO: Add the inherit function to the built-in `Object` object.
+    Object.inherit = inherit;
+} ();
 // SIG // Begin signature block
 // SIG // MIIaVgYJKoZIhvcNAQcCoIIaRzCCGkMCAQExCzAJBgUr
 // SIG // DgMCGgUAMGcGCisGAQQBgjcCAQSgWTBXMDIGCisGAQQB
